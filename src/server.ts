6afd52c053e8895b359a9cd87a0c2542df758be6
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-07-04 02:02:50 -0300

    Firt rote

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json())
+
+app.get('/test', (request, response) => {
+  return response.json({ message: 'Hello world'})
+})
 
 app.listen(3000, () => {
   console.log('Server is running 🚀');
